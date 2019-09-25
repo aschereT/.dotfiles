406d004479c4d2ee120d729ac197aaf1da347c97
Vincent Tan <cryptchrysaetos@gmail.com>
2019-09-25 06:31:48 +0000

    fix

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-[ -f ~/.zshrc ] && rm -f ~/.zshrc && ln -s ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
-[ -f ~/.gitconfig ] && rm -f ~/.gitconfig && ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
+[ -f ~/.zshrc ] && rm -f ~/.zshrc; ln -s ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
+[ -f ~/.gitconfig ] && rm -f ~/.gitconfig; ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
 cd && git clone --depth=1 --recurse-submodules https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
