cfb4f666b2184175fd7307d3a6f16801ac9ac8f8
Vincent Tan <cryptchrysaetos@gmail.com>
2019-10-03 06:34:53 +0000

    Install nano syntax highlighting

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 [ -f ~/.zshrc ] && rm -f ~/.zshrc; ln -s ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
 [ -f ~/.gitconfig ] && rm -f ~/.gitconfig; ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
 cd && git clone --depth=1 --recurse-submodules https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
