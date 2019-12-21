e88a6faa735549cd89f5e16dc173e17f174e989d
Vincent Tan <cryptchrysaetos@gmail.com>
2019-12-20 20:59:21 -0800

    Further fixes

---
+++ scripts/alacritty.sh
@@ -0,0 +1,2 @@
+mkdir -p $HOME/.config/alacritty
+ln -s `pwd`/../other-configs/alacritty.yml $HOME/.config/alacritty/alacritty.yml
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,11 @@
+alias gitc='git clone --depth=1 --recurse-submodules -j`nproc`'
+
 [ -f ~/.zshrc ] && rm -f ~/.zshrc; ln -s ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
 [ -f ~/.gitconfig ] && rm -f ~/.gitconfig; ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
-git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-cd && git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
+gitc https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+gitc https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+gitc https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+cd $HOME && gitc https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
