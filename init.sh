4cd408a2a69030a41a1000dd5e7ac3d366aa3d7f
Vincent Tan <cryptchrysaetos@gmail.com>
2019-12-20 16:29:06 -0800

    Fix path
    Signed-off-by: Vincent Tan <cryptchrysaetos@gmail.com>

--- init.sh
+++ init.sh
@@ -2,6 +2,10 @@
 [ -f ~/.gitconfig ] && rm -f ~/.gitconfig; ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
 git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+<<<<<<< Updated upstream
 git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+=======
+git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+>>>>>>> Stashed changes
 cd && git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
