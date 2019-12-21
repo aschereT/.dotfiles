5ee7a7a40e0baa050b56df3549af5a1e12862073
Vincent Tan <cryptchrysaetos@gmail.com>
2019-12-20 16:24:42 -0800

    Forget about git modules, just clone it
    Signed-off-by: Vincent Tan <cryptchrysaetos@gmail.com>

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 [ -f ~/.zshrc ] && rm -f ~/.zshrc; ln -s ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
 [ -f ~/.gitconfig ] && rm -f ~/.gitconfig; ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
-cd && git clone --depth=1 --recurse-submodules https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
+git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+cd && git clone --depth=1 --recurse-submodules -j`nproc` https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
