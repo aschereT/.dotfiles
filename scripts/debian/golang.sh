d195c54c2d35634e7de0236dbcccc085aa838c6a
Vincent Tan <cryptchrysaetos@gmail.com>
2020-01-09 14:19:15 -0800

    Improve golang.sh

--- scripts/debian/golang.sh
+++ scripts/debian/golang.sh
@@ -1,3 +1,3 @@
-sudo add-apt-repository ppa:longsleep/golang-backports
-sudo apt-get update
-sudo apt-get install golang-go
+sudo add-apt-repository ppa:longsleep/golang-backports && \
+sudo apt update && \
+sudo apt install golang-go
