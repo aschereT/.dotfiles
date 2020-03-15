afa110fd06d1a4c2450e2ef6e3f02a12d477fb1b
Vincent Tan <vincent@cto.ai>
2020-03-15 00:08:16 -0700

    Try to fix actions
    Signed-off-by: Vincent Tan <vincent@cto.ai>

---
+++ scripts/fedora/kitty.sh
@@ -0,0 +1,1 @@
+sudo dnf install python3-devel harfbuzz-devel zlib-devel libpng-devel freetype-devel fontconfig-devel libXcursor-devel libXrandr-devel glfw-devel libXinerama-devel libxkbcommon-x11-devel mesa-libGL-devel libXi-devel wayland-devel python3-sphinx dbus-devel gcc libcanberra-devel
