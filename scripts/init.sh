08a943d4077aac17a314e398c4e3f8ff70666519
Vincent Tan <cryptchrysaetos@gmail.com>
2020-05-11 21:26:07 -0700

    Fix commit
    Signed-off-by: Vincent Tan <cryptchrysaetos@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,4 +14,4 @@ curl -s https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
 #install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p "$HOME/.config/nvim/" && ln -s "$HOME/.oh-my-zsh/custom/other-configs/nvim/" "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/" && ln -s "$HOME/.oh-my-zsh/custom/other-configs/nvim/" "$HOME/.config/nvim"
