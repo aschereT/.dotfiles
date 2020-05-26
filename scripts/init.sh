2a0dc97ece280e66150363bac9a1eaefa329c650
Vincent Tan <vtan@policyreporter.com>
2020-05-25 20:34:24 -0700

    Move zshcustom directory out of ~/.oh-my-zsh
    Signed-off-by: Vincent Tan <vtan@policyreporter.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 git submodule update --init --recursive && git submodule update --remote --merge
-[ -f ~/.zshrc ] && rm -f ~/.zshrc; ln -s ~/.oh-my-zsh/custom/.zshrc ~/.zshrc
-[ -f ~/.gitconfig ] && rm -f ~/.gitconfig; ln -s ~/.oh-my-zsh/custom/.gitconfig ~/.gitconfig
+[ -f $HOME/.zshrc ] && rm -f $HOME/.zshrc; ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+[ -f $HOME/.gitconfig ] && rm -f $HOME/.gitconfig; cp $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 cd $HOME && gitc https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
 curl -s https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
 
@@ -14,5 +14,5 @@ curl -s https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | zsh
 #install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir -p "$HOME/.config/" && ln -s "$HOME/.oh-my-zsh/custom/other-configs/nvim/" "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/" && ln -s "$HOME/.dotfiles/other-configs/nvim/" "$HOME/.config/nvim"
 nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
