5051349b39521404927b71beb54e5046d604f3d2
Vincent Tan <vtan@policyreporter.com>
2020-06-08 15:44:39 -0700

    Shellcheck rust.sh
    Signed-off-by: Vincent Tan <vtan@policyreporter.com>

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -4,7 +4,7 @@ alias gitc='git clone --depth=1 --recurse-submodules --shallow-submodules --sing
 alias gcle='git reset --hard; git clean -fd'
 if ! [ -x "$(command -v rustc)" ]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | zsh -s -- --default-toolchain nightly --profile=default -y
-  . $HOME/.cargo/env
+  . "$HOME"/.cargo/env
   mkdir ~/.zfunc/
   rustup completions zsh > ~/.zfunc/_rustup
 fi
@@ -14,36 +14,35 @@ rustup update
 
 #export RUSTFLAGS="-C target-cpu=native -C target-feature=-avx512bw,-avx512cd,-avx512dq,-avx512er,-avx512f,-avx512ifma,-avx512pf,-avx512vbmi,-avx512vl"
 export RUSTFLAGS="-C opt-level=3 -C target-cpu=native"
-mkdir -p $HOME/repos
-cd $HOME/repos
+mkdir -p "$HOME"/repos
+cd "$HOME"/repos || exit
 
 if [ ! -d "fd" ]; then gitc https://github.com/sharkdp/fd.git; fi
 cd fd && \
 gcle && git pull && git gc --prune=all --aggressive && \
 cargo update && \
 cargo install -f --all-features --bins --path .
-cd $HOME/repos
+cd "$HOME"/repos || exit
 
 if [ ! -d "ripgrep" ]; then gitc https://github.com/BurntSushi/ripgrep.git; fi
 cd ripgrep && \
 gcle && git pull && git gc --prune=all --aggressive && \
 cargo update && \
-cat Cargo.toml | sed "s/debug = 1/debug = 0/g" > Cargo.toml2 && mv Cargo.toml2 Cargo.toml && \
+sed "s/debug = 1/debug = 0/g" < Cargo.toml > Cargo.toml2 && mv Cargo.toml2 Cargo.toml && \
 cargo install -f --features 'simd-accel' --bins --path .
-cd $HOME/repos
+cd "$HOME"/repos || exit
 
 if [ ! -d "lsd" ]; then gitc https://github.com/Peltoche/lsd.git; fi
 cd lsd && \
 gcle && git pull && git gc --prune=all --aggressive && \
 cargo update && \
 cargo install -f --all-features --bins --path .
-cd $HOME/repos
+cd "$HOME"/repos || exit
 
 if [ ! -d "bat" ]; then gitc https://github.com/sharkdp/bat.git; fi
 cd bat && \
 gcle && git pull && git gc --prune=all --aggressive && \
 cargo update && \
-cat Cargo.toml | sed "s/debug = 1/debug = 0/g" > Cargo.toml2 && mv Cargo.toml2 Cargo.toml && \
+sed "s/debug = 1/debug = 0/g" < Cargo.toml > Cargo.toml2 && mv Cargo.toml2 Cargo.toml && \
 cargo install -f --all-features --bins --path .
-cd $HOME/repos
-
+cd "$HOME"/repos || exit
